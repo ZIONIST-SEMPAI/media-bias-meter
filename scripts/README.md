ee5f635ea8f732cf0439861983969f7e67fcb546
Twistedseed <twisted.seed00@gmail.com>
2024-02-24 05:11:54 +0000

    feat: scrape script

---
+++ scripts/README.md
@@ -0,0 +1,71 @@
+# NYT scraper
+
+This Python script is designed to scrape articles from The New York Times (NYT) and store them in a MySQL database. It automates the collection of articles, efficiently gathering data without manual intervention.
+
+## Features
+
+- Automated scraping of NYT articles.
+- Efficient storage of articles in a MySQL database.
+- Configurable for specific sections or topics.
+- Easy to set up and run on any system with Python and MySQL.
+- Provides a foundation for further data analysis and research.
+
+## Getting Started
+
+### Prerequisites
+- Python 3.10.0 or greater
+- MySQL instance 
+- New York Times(NYT) API-KEY follow this [link](https://developer.nytimes.com/get-started) to get started 
+- .env file under the scripts directory with the following secrets 
+
+```.env
+API_KEY=<NTY-API-KEY>
+DB_HOST=localhost
+DB_USER=<db-username>
+DB_PASS=<db-user-password>
+DB_NAME=media_articles
+```
+
+### Installation
+
+A step-by-step series of examples that tell you how to get a development environment running.
+
+#### Setting Up a Virtual Environment
+
+To isolate the project dependencies, it's recommended to use a virtual environment. Run the following command in your terminal to create a virtual environment in the current directory:
+
+```bash
+python -m venv venv
+```
+
+#### Activating the Virtual Environment
+
+After creating the virtual environment, you need to activate it. Use the appropriate command for your operating system.
+
+**Windows:**
+
+```bash
+.\venv\Scripts\Activate.ps1
+```
+
+**macOS/Linux:**
+
+```bash
+source venv/bin/activate
+```
+
+#### Installing Dependencies
+
+Install all dependencies that are required for the project to run.
+
+```bash
+pip install -r requirements.txt
+```
+
+### Running the Script
+
+Explain how to run your script, including any command-line arguments you can use.
+
+```bash
+python your_script.py
+```
---
+++ scripts/scrape.py
@@ -0,0 +1,155 @@
+import os
+import requests
+import mysql.connector
+from pynytimes import NYTAPI
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+# load environment secrets from .env file
+load_dotenv()
+
+# retrieve environment variables
+API_KEY = os.getenv("API_KEY")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+
+def get_articles(API_KEY) -> list:
+  nyt = NYTAPI(API_KEY, parse_dates=True)
+
+  articles = nyt.article_search(
+      results = 10,
+      
+      options = {
+      "sort": "newest",
+      "fq": "Gaza AND Palestine AND Israel AND Palestinian AND Israeli",
+      })
+
+
+  # New list to hold the extracted info
+  extracted_info = []
+
+  # Loop through each article in the list
+  for article in articles:
+    # Extract info from the json
+    web_url = article.get('web_url', None) 
+    source = article.get('source', None)  
+    headline_main = article.get('headline', {}).get('main', None)
+    byline = article.get('byline', {}).get('original', None)
+
+    # Append the extracted information to the new list as a dictionary
+    extracted_info.append({'headline': headline_main, 'byline': byline, 'source': source, 'web_url': web_url})
+  
+
+  return extracted_info
+
+def get_article_content(article_url: str) -> str:
+  if not article_url:
+    print("No article URL provided")
+    return "No article URL provided"
+
+  print(f"Fetching article content for {article_url}")
+
+  # Check if the article is available on the Wayback Machine
+  url = "http://archive.org/wayback/available?url=" + article_url
+
+  payload = {}
+  headers = {}
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+
+  data = response.json()
+
+  # Check if the article is available on the Wayback Machine
+
+  if data.get('archived_snapshots'):
+    # Get the closest snapshot
+    snapshot = data['archived_snapshots'].get('closest', None)
+
+    if snapshot:
+      # Get the snapshot URL
+      snapshot_url = snapshot.get('url', None)
+
+      if snapshot_url:
+        # Get the article content
+        r = requests.get(snapshot_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        target_div = soup.find('section', class_='meteredContent css-1r7ky0e')
+
+        if target_div:
+          paragraphs = target_div.get_text(strip=True, separator='\n')
+          print("Success: Article content extracted successfully!")
+          return paragraphs
+        else:
+          print("failed: article content not found - div is missing")
+          return "No div found"
+      else:
+        print("failed: snapshot URL not found - article content not available on the Wayback Machine")
+        return "No snapshot URL found"
+    else:
+      print("failed: snapshot not found - article has not been archived")
+      return "No snapshot found"
+
+def save_to_db(article: dict):
+
+  try: 
+    # Connect to the database
+    db = mysql.connector.connect(
+      host=DB_HOST,
+      user=DB_USER,
+      password=DB_PASS,
+      database=DB_NAME
+    )
+    print("Connection established")
+  except mysql.connector.Error as err:
+    print(f"Failed to connect: {err}")
+
+  # Create a cursor
+  cursor = db.cursor()
+
+  # Create a new table
+  create_table = """
+  CREATE TABLE IF NOT EXISTS articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    headline VARCHAR(255),
+    byline VARCHAR(255),
+    source VARCHAR(255),
+    web_url TEXT,
+    body TEXT
+  )
+  """
+  cursor.execute(create_table)
+
+  # Prepare the SQL query
+  sql = "INSERT INTO articles (headline, byline, source, web_url, body) VALUES (%s, %s, %s, %s, %s)"
+  values = (article.get('headline'), article.get('byline'), article.get('source'), article.get('web_url'), article.get('body'))
+
+  # Execute the SQL query
+  cursor.execute(sql, values)
+
+  # Commit the changes
+  db.commit()
+
+  # Close the connection
+  db.close()
+
+  print(f"Article {article.get('headline')} saved to the database")
+
+
+
+
+if __name__ == "__main__":
+
+  # Get the articles metadata
+  articles = get_articles(API_KEY)
+
+  for article in articles:
+    # Get the article content
+    article["body"] = get_article_content(article.get('web_url'))
+
+    # Save the article to the database
+    save_to_db(article)
+
+
